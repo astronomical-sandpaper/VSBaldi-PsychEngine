ff9617a448b70f017bae7322b2c2bb4738741b9a
OwenMakesStuff <85309352+owendeveloper490@users.noreply.github.com>
2021-10-31 09:07:31 -0500

    im an actual idiot bruh

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -72,7 +72,7 @@ class StoryMenuState extends MusicBeatState
 		rankText.screenCenter(X);
 
 		var ui_tex = Paths.getSparrowAtlas('campaign_menu_UI_assets');
-		var bgYellow:FlxSprite = new FlxSprite(0, 56).makeGraphic(FlxG.width, 386, 0x10C311);
+		var bgYellow:FlxSprite = new FlxSprite(0, 56).makeGraphic(FlxG.width, 386, 0xFF11C411);
 		bgSprite = new FlxSprite(0, 56);
 		bgSprite.antialiasing = ClientPrefs.globalAntialiasing;
 
